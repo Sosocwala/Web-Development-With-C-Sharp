008c4b9d036861af09862e6fad7181f281d65e1f
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:12:57 +0100

    Using List Class

---
+++ Using List Class/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_List_Class
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
