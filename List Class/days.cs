9a34205cac508a2e9110b5bcee0123a4c79c8ce7
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:08:09 +0100

    List Class

---
+++ List Class/days.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Class
+{
+    public class days
+    {
+        public int dayNum { get; set; }
+        public string textValue { get; set; }
+    }
+}
