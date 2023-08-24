736c0d937e4c9cca30091d177b7ddc259e8ba596
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:13:29 +0100

    Using List Class

---
+++ Using List Class/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Using_List_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFillList = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cmbDays = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnFillList
+            // 
+            this.btnFillList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFillList.Location = new System.Drawing.Point(217, 82);
+            this.btnFillList.Name = "btnFillList";
+            this.btnFillList.Size = new System.Drawing.Size(97, 32);
+            this.btnFillList.TabIndex = 0;
+            this.btnFillList.Text = "Fill List";
+            this.btnFillList.UseVisualStyleBackColor = true;
+            this.btnFillList.Click += new System.EventHandler(this.btnFillList_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.Location = new System.Drawing.Point(12, 85);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(199, 253);
+            this.textOutput.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(217, 321);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 29);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cmbDays
+            // 
+            this.cmbDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDays.FormattingEnabled = true;
+            this.cmbDays.Location = new System.Drawing.Point(218, 121);
+            this.cmbDays.Name = "cmbDays";
+            this.cmbDays.Size = new System.Drawing.Size(121, 28);
+            this.cmbDays.TabIndex = 3;
+            this.cmbDays.SelectedIndexChanged += new System.EventHandler(this.cmbDays_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbDays);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.btnFillList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFillList;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cmbDays;
+    }
+}
+
