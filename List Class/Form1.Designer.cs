81e4c69aad6ce15f756e059d0b01b48d6205f4a4
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:09:17 +0100

    List Class

---
+++ List Class/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace List_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFillList = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cmbDays = new System.Windows.Forms.ComboBox();
+            this.gridList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFillList
+            // 
+            this.btnFillList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFillList.Location = new System.Drawing.Point(218, 73);
+            this.btnFillList.Name = "btnFillList";
+            this.btnFillList.Size = new System.Drawing.Size(94, 29);
+            this.btnFillList.TabIndex = 0;
+            this.btnFillList.Text = "Fill List";
+            this.btnFillList.UseVisualStyleBackColor = true;
+            this.btnFillList.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.Location = new System.Drawing.Point(12, 73);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(200, 365);
+            this.textOutput.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(231, 407);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 31);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cmbDays
+            // 
+            this.cmbDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDays.FormattingEnabled = true;
+            this.cmbDays.Location = new System.Drawing.Point(218, 108);
+            this.cmbDays.Name = "cmbDays";
+            this.cmbDays.Size = new System.Drawing.Size(121, 28);
+            this.cmbDays.TabIndex = 3;
+            this.cmbDays.SelectedIndexChanged += new System.EventHandler(this.cmbDays_SelectedIndexChanged);
+            // 
+            // gridList
+            // 
+            this.gridList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridList.Location = new System.Drawing.Point(218, 174);
+            this.gridList.Name = "gridList";
+            this.gridList.Size = new System.Drawing.Size(170, 227);
+            this.gridList.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridList);
+            this.Controls.Add(this.cmbDays);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.btnFillList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFillList;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cmbDays;
+        private System.Windows.Forms.DataGridView gridList;
+    }
+}
+
