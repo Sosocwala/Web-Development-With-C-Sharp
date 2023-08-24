c46ec537351886999073a2c1b59e8b465cd4c2f6
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:08:30 +0100

    List Class

---
+++ List Class/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Class
+{
+   public class Employee
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+    }
+}
