de88490432d13a13eefba14f5affa18f74d0e1af
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-24 21:12:50 +0100

    Using List Class

---
+++ Using List Class/days.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_List_Class
+{
+   public class days
+    {
+        public int ID { get; set; }
+        public string tyextValue { get; set; }
+    }
+}
