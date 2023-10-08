6df75d267afbacb74c2744a844763c88005ce805
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-08 22:33:16 +0100

    Encapsulation

---
+++ Encapsulation/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Employee
+    {
+        protected int ID { get; set; }
+        public string name { get; set; }
+        public string suname { get; set; }
+        public int Age { get; set; }
+        public int cellNumber { get; set; }
+    }
+}
