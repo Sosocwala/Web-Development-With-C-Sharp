1aee9860bda62919672b70d4684d7e1a48068aa6
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-08 22:30:53 +0100

    Using Static

---
+++ Using Static/student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Static
+{
+    class student
+    {
+        public static void sellingCrown(string text)
+        {
+
+        }
+         
+        public static int studentIDNo { get; set; }
+        public static string studentName { get; set; }
+        public static int cellNumber { get; set; }
+    }
+   
+}
