510080461cef0270fcd12fb63ed90af633aa5b48
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-09 21:25:46 +0100

    Inheritance

---
+++ Inheritance/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Teacher : Employee
+    {
+        public string Branch { get; set; }
+
+        public void findSalary()
+        {
+            Salary = 2000;
+        }
+       public void setValue2()
+        {
+            position = "Director";
+            Branch = "Water&Sanitation";
+        }
+    }
+}
