9304c551165e42ba9ee5163cac2c2a2c44dc474b
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-09 21:26:45 +0100

    Interface

---
+++ Interface/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(12, 70);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(211, 277);
+            this.textOutput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(260, 314);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 33);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Interface/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        interface Computer
+        {
+            void getDekstop();
+            void getLaptop();
+        }
+        interface Computers
+        {
+            void getPrice();
+        }
+        class DellComputers : Computer, Computers
+        {
+            public string name { get; set; }
+            public string Model { get; set; }
+            public int Price { get; set; }
+            public void getDekstop()
+            {
+                name = "Dell";
+                Model = "Icore";
+            }
+
+           public void getLaptop()
+            {
+                name = "Mackbook";
+                Model = "IoS";
+            }
+            public void getPrice()
+            {
+                Price = 2500; 
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            DellComputers DellComputer = new DellComputers();
+            DellComputer.getDekstop();
+            DellComputer.getPrice();
+            string allvalues = "Name: " + DellComputer.name + Environment.NewLine;
+            allvalues += "Model: " + DellComputer.Model + Environment.NewLine;
+            allvalues += "Price: " + DellComputer.Price + Environment.NewLine;
+            textOutput.Text = allvalues;
+          
+        }
+    }
+}
