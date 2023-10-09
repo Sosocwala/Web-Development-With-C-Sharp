8fbd3a51e50475631b3f664b9965f88af494c2d1
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-09 21:24:55 +0100

    Inheritance

---
+++ Inheritance/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Human
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public char Gender { get; set; }
+        public int Age { get; set; }
+
+    }
+}
