e14d215e56390d963fd8a355e2ed2a922f9a73cc
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-09 21:23:36 +0100

    Inheritance

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Employee : Human
+    {
+        public string position { get; set; }
+        public decimal Salary { get; set; }
+
+        public void setValue()
+        {
+            ID = 12345234;
+            name = "Mango";
+            surname = "Suthu";
+            Gender = 'M';
+            Age = 34;
+        }
+    }
+}
