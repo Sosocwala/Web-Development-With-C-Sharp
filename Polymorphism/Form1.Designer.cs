ef04d1df4b112eb3efc86f06d2a37bb84e55cb35
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-12 21:26:11 +0100

    Polymorphism

---
+++ Polymorphism/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Polymorphism
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.btnpara2 = new System.Windows.Forms.Button();
+            this.btn2para = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(265, 62);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(131, 34);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "1 parameter";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.Location = new System.Drawing.Point(12, 62);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(247, 336);
+            this.textOutput.TabIndex = 1;
+            // 
+            // btnpara2
+            // 
+            this.btnpara2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpara2.Location = new System.Drawing.Point(265, 102);
+            this.btnpara2.Name = "btnpara2";
+            this.btnpara2.Size = new System.Drawing.Size(131, 34);
+            this.btnpara2.TabIndex = 0;
+            this.btnpara2.Text = "2 parameter";
+            this.btnpara2.UseVisualStyleBackColor = true;
+            this.btnpara2.Click += new System.EventHandler(this.btnpara2_Click);
+            // 
+            // btn2para
+            // 
+            this.btn2para.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2para.Location = new System.Drawing.Point(265, 142);
+            this.btn2para.Name = "btn2para";
+            this.btn2para.Size = new System.Drawing.Size(131, 34);
+            this.btn2para.TabIndex = 0;
+            this.btn2para.Text = "3 parameter";
+            this.btn2para.UseVisualStyleBackColor = true;
+            this.btn2para.Click += new System.EventHandler(this.btn2para_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(265, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 34);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "4 parameter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(265, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 34);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "5 parameter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(265, 262);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 34);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "virtual";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(265, 302);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 34);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Override";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(265, 342);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(131, 34);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Exit";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn2para);
+            this.Controls.Add(this.btnpara2);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Button btnpara2;
+        private System.Windows.Forms.Button btn2para;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Polymorphism/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Teacher teacher1 = new Teacher();
+            teacher1.setValue(12);
+            string result = teacher1.getValues();
+            textOutput.Text = result;
+        }
+
+        private void btnpara2_Click(object sender, EventArgs e)
+        {
+            Teacher teacher2 = new Teacher();
+            teacher2.setValue(12, "Managa");
+            string result = teacher2.getValues();
+            textOutput.Text = result;
+        }
+
+        private void btn2para_Click(object sender, EventArgs e)
+        {
+            Teacher teacher3 = new Teacher();
+            teacher3.setValue(12, "Managa", "Zwelidinga");
+            string result = teacher3.getValues();
+            textOutput.Text = result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Teacher teacher4 = new Teacher();
+            teacher4.setValue(12, "Managa", "Zwelidinga", 20000);
+            string result = teacher4.getValues();
+            textOutput.Text = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Teacher teacher5 = new Teacher();
+            teacher5.setValue(12, "Managa", "Zwelidinga", 20000, "Admin Clerck");
+            string result = teacher5.getValues();
+            textOutput.Text = result;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Teacher2 teacher11 = new Teacher2();
+            string result = teacher11.setValue(11, "Mlibo", "Zondo", 20000, "Teacher");
+            textOutput.Text = result;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            employee2 teacher10 = new employee2();
+            string result = teacher10.setValue(11, "Mlibo", "Zondo", 20000);
+            textOutput.Text = result;
+        }
+    }
+}
