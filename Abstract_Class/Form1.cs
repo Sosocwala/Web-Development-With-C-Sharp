b8c356bad17e23fb455cd60c8fc57c9e3ef0bafc
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-10-12 21:28:39 +0100

    Abstract Class

---
+++ Abstract_Class/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Abstract_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        abstract class Vehicle
+        {
+            public abstract double getFuelAmount();
+        }
+        class Bus : Vehicle
+        {
+            public override double getFuelAmount()
+            {
+                return 20000;
+            }
+        }
+        class Truck : Bus
+        {
+            public override double getFuelAmount()
+            {
+                return 4000;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string text = "";
+            Vehicle vs;
+            vs = new Bus();
+            double busFuel = vs.getFuelAmount();
+            text += "Bus Fuel Amount is: " + busFuel.ToString() + Environment.NewLine;
+            vs = new Truck();
+            double truckFuel = vs.getFuelAmount();
+            text += "Truck Fuel Amount is: " + truckFuel.ToString() + Environment.NewLine;
+            textOutput.Text = text;    
+        }
+    }
+}
---
+++ Partial ve Sealed Class/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Partial_ve_Sealed_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public partial class Employee
+        {
+            public int ID { get; set; }
+            public string name { get; set; }
+            public string surname { get; set; }
+            public int age { get; set; }
+        }
+        public partial class Employee
+        {
+            public void setValues()
+            {
+                ID = 123;
+                name = "Malabala";
+                surname = "Zankonde";
+                age = 32;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.setValues();
+            string resultText = "";
+            resultText += "ID: " + employee.ID.ToString() + Environment.NewLine;
+            resultText += "Name: " + employee.name + Environment.NewLine;
+            resultText += "Surname: " + employee.surname + Environment.NewLine;
+            resultText += "Age: " + employee.age.ToString() + Environment.NewLine;
+            textOutput.Text = resultText;
+        }
+    }
+}
