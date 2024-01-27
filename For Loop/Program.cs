32c22126d9fcee92ac1c589429df712a06f6e691
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2024-01-27 23:59:39 +0200

    For loop

--- For Loop/Program.cs
+++ For Loop/Program.cs
@@ -17,6 +17,5 @@ namespace For_Loop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
     }
 }
